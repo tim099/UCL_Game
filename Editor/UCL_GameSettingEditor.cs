3b333160b89849aa096e3d7a71b9f2ad3e97d339
tim1125 <tim11251994@gmail.com>
2020-04-05 01:52:41 +0800

    Add UCL_GameSetting.cs

---
+++ Editor/UCL_GameSettingEditor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace UCL.GameLib {
+    [CustomEditor(typeof(UCL_GameSetting), true)]
+    public class UCL_GameSettingEditor : Editor {
+        public override void OnInspectorGUI() {
+            base.OnInspectorGUI();
+            var controller = (UCL_GameSetting)target;
+        }
+    }
+}
---
+++ UCL_GameScript/UCL_GameSetting.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UCL.GameLib {
+    [CreateAssetMenu(fileName = "New GameSetting", menuName = "UCL/GameSetting")]
+    public class UCL_GameSetting : ScriptableObject {
+
+    }
+}
