0696b9e84ca7d943b36c5fd81fa70fee1a468cde
tim099 <tim11251994@gmail.com>
2020-04-13 17:10:02 +0800

    Move GameSetting.asset into Resources

--- UCL_GameScript/UCL_GameSetting.cs
+++ UCL_GameScript/UCL_GameSetting.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 namespace UCL.GameLib {
     [CreateAssetMenu(fileName = "New GameSetting", menuName = "UCL/GameSetting")]
     public class UCL_GameSetting : ScriptableObject {
-
+        static public UCL_GameSetting Get() {
+            return Resources.Load<UCL_GameSetting>("GameSetting");
+        }
+#if UNITY_EDITOR
+        [UnityEditor.MenuItem("UCL/GameLib/GameSetting")]
+        static public void OpenSceneSwitcher() {
+            //Selection.activeObject = AssetDatabase.LoadMainAssetAtPath("Assets/Libs/UCL_Modules/UCL_Scene/SceneSwitcher.asset");
+            UnityEditor.Selection.activeObject = Get();
+        }
+#endif
     }
 }
