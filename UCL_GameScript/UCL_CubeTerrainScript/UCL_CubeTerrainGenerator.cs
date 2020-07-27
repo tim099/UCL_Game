62dc5da32bd6764e896688c230c9160ac37f4f8c
tim099 <tim11251994@gmail.com>
2020-07-27 17:42:15 +0800

    Add UCL_CubeTerrainGenerator.cs

---
+++ UCL_GameScript/UCL_CubeTerrainScript/UCL_CubeTerrainGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UCL.GameLib.CubeTerrain {
+    public class UCL_CubeTerrainGenerator : MonoBehaviour {
+
+    }
+}
+
